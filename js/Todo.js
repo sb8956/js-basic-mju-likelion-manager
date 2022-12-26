e1f6cc8962381878383e7610ddbe6afa975988af
yhyem <yoonhyemin0120@gmail.com>
2022-12-27 00:57:12 +0900

    feat: localStorage 구현

--- js/Todo.js
+++ js/Todo.js
@@ -1,47 +1,71 @@
-var TodoArr = [];
+const todoList = document.getElementById('todoList');
+let todoLists = [];
+
+init();
 
 function init() {
-    
+  const loadTodo = localStorage.getItem("todo");
+  if (loadTodo !== null) {
+    const loadTodos = JSON.parse(loadTodo);
+    loadTodos.forEach(function (data) {
+      displayTodo(data);
+      todoLists.push(data);
+    });
+  }
 }
 
-function addName() {
-    var inputValue = document.getElementById('nameInput');
-    var title = document.getElementById('title');
+function displayTodo(todo) {
+  var wrapList = document.createElement('div');
+  var list = document.createElement('li');
+  var removeBtn = document.createElement('button');
 
-    localStorage.setItem("name", inputValue.value);
-    title.innerHTML = localStorage.getItem("name")+'의 TodoList';
-    inputValue.style.display='none';
+  wrapList.classList.add('wrapList');
+  todoList.appendChild(wrapList);
+
+  list.innerText = todo.todo;
+  wrapList.appendChild(list);
+  removeBtn.innerText = 'x';
+  wrapList.appendChild(removeBtn);
+  removeBtn.classList.add('removeBtn');
+  removeBtn.id = todo.id;
+
+  list.addEventListener('click', function () {
+    list.style.color = "gray"
+    list.style.textDecoration = "line-through";
+  })
+  removeBtn.addEventListener('click', deleteTodo);
 }
 
 function addTodo() {
-    var inputValue = document.getElementById('todoInput');
-    var todoList = document.getElementById('todoList');
+  var inputValue = document.getElementById('todoInput');
+  const newId = todoLists.length + 1;
 
-    var wrapList = document.createElement('div');
-    wrapList.classList.add('wrapList');
-    todoList.appendChild(wrapList);
+  if (inputValue.value == "")
+    alert('todo를 추가해주세요.');
 
-    if (!inputValue.value==null)
-        alert('todo를 추가해주세요.');
-    else {
-        var list = document.createElement('li');
-        var removeBtn = document.createElement('button');
-        list.innerText = inputValue.value;
-        wrapList.appendChild(list);
-        removeBtn.innerText = 'x';
-        wrapList.appendChild(removeBtn);
-        removeBtn.classList.add('removeBtn');
+  else {
+    const todoObj = {
+      todo: inputValue.value,
+      id: newId,
+    };
 
-        inputValue.value = "";
-    }
+    displayTodo(todoObj);
 
-    list.addEventListener('click', function () {
-        list.style.color = "gray"
-        list.style.textDecoration = "line-through";
-    })
-    removeBtn.addEventListener('click', function () {
-        todoList.removeChild(wrapList);
-        wrapList.removeChild(list);
-        wrapList.removeChild(removeBtn);
-    })
+    todoLists.push(todoObj);
+    localStorage.setItem("todo", JSON.stringify(todoLists));
+    inputValue.value = "";
+  }
+}
+
+function deleteTodo(event) {
+  const btn = event.target;
+  const wrapList = btn.parentNode;
+  const todolist = document.getElementById('todoList');
+
+  todolist.removeChild(wrapList);
+  const removeTodo = todoLists.filter(function (todo) {
+    return todo.id !== parseInt(btn.id);
+  });
+
+  localStorage.setItem("todo", JSON.stringify(removeTodo));
 }
