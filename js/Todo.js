c8dd48a5c75884bb8c7077b7059b4aacca8430d5
yhyem <yoonhyemin0120@gmail.com>
2022-12-26 22:52:25 +0900

    feat: 로컬 스토리지 사용자 이름 저장

--- js/Todo.js
+++ js/Todo.js
@@ -1,18 +1,36 @@
+var TodoArr = [];
+
+function init() {
+    
+}
+
+function addName() {
+    var inputValue = document.getElementById('nameInput');
+    var title = document.getElementById('title');
+
+    localStorage.setItem("name", inputValue.value);
+    title.innerHTML = localStorage.getItem("name")+'의 TodoList';
+    inputValue.style.display='none';
+}
 
 function addTodo() {
     var inputValue = document.getElementById('todoInput');
     var todoList = document.getElementById('todoList');
 
-    var list = document.createElement('li');
-    var removeBtn = document.createElement('button');
+    var wrapList = document.createElement('div');
+    wrapList.classList.add('wrapList');
+    todoList.appendChild(wrapList);
 
-    if (!inputValue.value)
+    if (!inputValue.value==null)
         alert('todo를 추가해주세요.');
     else {
+        var list = document.createElement('li');
+        var removeBtn = document.createElement('button');
         list.innerText = inputValue.value;
-        todoList.appendChild(list);
+        wrapList.appendChild(list);
         removeBtn.innerText = 'x';
-        todoList.appendChild(removeBtn);
+        wrapList.appendChild(removeBtn);
+        removeBtn.classList.add('removeBtn');
 
         inputValue.value = "";
     }
@@ -22,7 +40,8 @@ function addTodo() {
         list.style.textDecoration = "line-through";
     })
     removeBtn.addEventListener('click', function () {
-        todoList.removeChild(list);
-        todoList.removeChild(removeBtn);
+        todoList.removeChild(wrapList);
+        wrapList.removeChild(list);
+        wrapList.removeChild(removeBtn);
     })
 }
