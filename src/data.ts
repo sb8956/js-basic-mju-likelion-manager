e96c80e1d95bd413b93581b6ec97512038de3430
yhyem <yoonhyemin0120@gmail.com>
2023-01-06 10:52:52 +0900

    feat: typescript 적용

--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,4 @@
-import {Todo} from './types/types'
+import {Todo} from './types/types.js'
 export let todoLists:Array<Todo> = [];
 export const saveTodoLists = (data:Array<Todo>) => {
     todoLists = data;
